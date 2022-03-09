35023b390497ae0896b93042a8ed752a8b167608
Avily Silva <avily.sva@gmail.com>
2022-03-09 11:18:25 -0300

    responsive font-size library was added

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header
+} from "./styles";
 
 export function Dashboard(): JSX.Element {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header />
     </Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,14 @@
+import { RFPercentage } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 import theme from "../../global/styles/theme";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.regular};
-  font-size: 26px;
-  color: ${({ theme }) => theme.colors.title};
+export const Header = styled.View`
+  height: ${RFPercentage(42)}px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
