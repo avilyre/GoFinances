aa5dc9f5150f0c8f89f4f277ab907efafa40d13e
Avily Silva <avily.sva@gmail.com>
2022-03-10 11:36:37 -0300

    logout icon was added to user profile

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,14 +1,35 @@
 import React from "react";
+import { Feather } from "@expo/vector-icons";
 
 import {
   Container,
-  Header
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreetings,
+  UserName,
+  LogoutIcon
 } from "./styles";
 
 export function Dashboard(): JSX.Element {
   return (
     <Container>
-      <Header />
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{
+              uri: "https://avatars.githubusercontent.com/u/66757451?v=4",
+            }} />
+            <User>
+              <UserGreetings>Olá,</UserGreetings>
+              <UserName>Avily Silva</UserName>
+            </User>
+          </UserInfo>
+          <LogoutIcon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,7 @@
-import { RFPercentage } from "react-native-responsive-fontsize";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
+
 import theme from "../../global/styles/theme";
 
 export const Container = styled.View`
@@ -11,4 +13,50 @@ export const Header = styled.View`
   height: ${RFPercentage(42)}px;
   width: 100%;
   background-color: ${({ theme }) => theme.colors.primary};
+
+  justify-content: center;
+`;
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Photo = styled.Image`
+  height: ${RFValue(48)}px;
+  width: ${RFValue(48)}px;
+
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+export const UserGreetings = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const LogoutIcon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `;
