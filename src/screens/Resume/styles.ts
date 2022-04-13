86addeb75f1de1face41a4b34844ee062dc5bed7
Avily Silva <avily.sva@gmail.com>
2022-04-13 11:42:45 -0300

    resume cards list padding adjusted

--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -1,10 +1,15 @@
-import styled from "styled-components/native";
 import { FlatList, FlatListProps } from "react-native";
+import styled from "styled-components/native";
+
 import { ResumeCategoryData } from "./interface";
 
 export const ResumeCardsList = styled(
   FlatList as new (props: FlatListProps<ResumeCategoryData>) => FlatList<ResumeCategoryData>
-)`
+).attrs({
+  contentContainerStyle: {
+    padding: 24
+  }
+})`
   flex: 1;
   
   width: 100%;
